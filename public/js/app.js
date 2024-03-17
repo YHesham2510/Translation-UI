da6b20a3b2cd0e30e4d4b269a7ae5718f5e0d6da
abdelrahman ashraf <abdelrahman.ashraf@seoudisupermarket.com>
2024-03-17 14:30:19 +0200

    initial

--- public/js/app.js
+++ public/js/app.js
@@ -1,71 +1,86 @@
-const itemsPerPage = 5;
-let currentPage = 1;
+document.addEventListener("DOMContentLoaded", function () {
+    document
+        .getElementById("exportButton")
+        .addEventListener("click", function () {
+            exportToCsv("translate_users.csv");
+        });
 
-function displayTableRows() {
-    const tableBody = document.getElementById("table_body");
-    tableBody.innerHTML = "";
+    function exportToCsv(filename) {
+        const table = document.getElementById("table_output");
+        const header = [
+            "ID",
+            "Item Code",
+            "Arabic Translation",
+            "English Translation",
+            "Username",
+        ];
+        const rows = [header];
 
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const displayedItems = display.slice(startIndex, endIndex);
+        for (const row of table
+            .getElementsByTagName("tbody")[0]
+            .getElementsByTagName("tr")) {
+            const rowData = [];
 
-    displayedItems.forEach((item) => {
-        const row = document.createElement("tr");
-        // console.log(item.is_updated==1);
-        if (item.is_updated == 1) {
-            row.classList.add("table-secondary");
-            // console.log(row.classList);
-            row.innerHTML = `
-          <td id="wanted_id">${item.id}</td>
-          <td id="wanted_item_code">${item.item_code}</td>
-          <td id="wanted_arabic_translation"><textarea class="table-secondary form-control" rows="8" cols="120" disabled style=resize:none>${item.arabic_translation}</textarea></td>
-          <td id="wanted_english_translation"><textarea class="table-secondary form-control" rows="8" cols="120" disabled style=resize:none>${item.english_translation}</textarea></td>
-          <td id="wanted_username">${item.username}</td>
-          <td>
-            <button class="btn btn-success" disabled onclick="handleEditButtonClick(this.parentNode.parentNode)">Edit</button>
-            <button class="btn btn-primary" disabled onclick="handleSaveButtonClick(this.parentNode.parentNode)">Save</button>
-            <button class="btn btn-primary" onclick="viewDescription(this.parentNode.parentNode)">View</button>
-          </td>
-        `;
-        } else {
-            row.innerHTML = `
-        <td id="wanted_id">${item.id}</td>
-        <td id="wanted_item_code">${item.item_code}</td>
-        <td id="wanted_arabic_translation"><textarea class="form-control" rows="8" cols="120" disabled style=resize:none>${item.arabic_translation}</textarea></td>
-        <td id="wanted_english_translation"><textarea class="form-control" rows="8" cols="120" disabled style=resize:none>${item.english_translation}</textarea></td>
-        <td id="wanted_username">${item.username}</td>
-        <td>
-          <button class="btn btn-success" onclick="handleEditButtonClick(this.parentNode.parentNode)">Edit</button>
-          <button class="btn btn-primary" onclick="handleSaveButtonClick(this.parentNode.parentNode)">Save</button>
-          <button class="btn btn-primary" onclick="viewDescription(this.parentNode.parentNode)">View</button>
-        </td>
-      `;
+            // Fetch specific data using getElementById or any other method you prefer
+            const id = row.querySelector("#wanted_id").textContent.trim();
+            const itemCode = row
+                .querySelector("#wanted_item_code")
+                .textContent.trim();
+            const arabicTranslation = row
+                .querySelector("#wanted_arabic_translation")
+                .textContent.trim();
+            const englishTranslation = row
+                .querySelector("#wanted_english_translation")
+                .textContent.trim();
+            const username = row
+                .querySelector("#wanted_username")
+                .textContent.trim();
+            rowData.push(
+                id,
+                itemCode,
+                arabicTranslation,
+                englishTranslation,
+                username
+            );
+
+            rows.push(rowData);
         }
-        tableBody.appendChild(row);
-    });
-}
 
-function handleEditButtonClick(row) {
-    const textareas = row.querySelectorAll("textarea");
-    textareas.forEach((textarea) => {
-        textarea.disabled = false;
-        textarea.style.color = "black";
-        textarea.style.background = "white";
-    });
-}
+        const csvContent = Papa.unparse(rows, { encoding: "UTF-8" });
+        const blob = new Blob([String.fromCharCode(0xfeff), csvContent], {
+            type: "text/csv;charset=utf-8;",
+        });
+        const link = document.createElement("a");
+        if (link.download !== undefined) {
+            const url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = "hidden";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
 
-function handleSaveButtonClick(row) {
-    // row.style.backgroundColor = "lightgray";
-    const textareas = row.querySelectorAll("textarea");
-    textareas.forEach((textarea) => {
-        textarea.disabled = true;
-        textarea.style.background = "lightgray";
-        const ID = row.querySelector("#wanted_id").innerText;
-        console.log("Item ID is: " + ID);
-        const itemId = ID;
-        const updatedText = textarea.value;
-        console.log("Updated Text is " + updatedText);
-        fetch(`http://localhost:8000/api/get-boolean-value/${itemId}`)
+    function updateDatabaseText(itemId, updatedText, updatedBooleanValue) {
+        let url = `http://localhost:8000/api/update-text/${itemId}`;
+        console.log("itemID: " + itemId);
+        console.log(updatedBooleanValue);
+        let data = {
+            text: updatedText,
+            booleanValue: updatedBooleanValue,
+            // arabic: updatedArabic,
+        };
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-TOKEN": document.querySelector(
+                    'meta[name="csrf-token"]'
+                ).content,
+            },
+            body: JSON.stringify(data),
+        })
             .then((response) => {
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
@@ -73,191 +88,10 @@ function handleSaveButtonClick(row) {
                 return response.json();
             })
             .then((data) => {
-                let updatedBooleanValue = data.is_updated;
-                updatedBooleanValue = "1";
-                console.log("bol val: " + updatedBooleanValue);
-                updateDatabaseText(itemId, updatedText, updatedBooleanValue);
-                const editButton = row.querySelector(".btn-success");
-                const saveButton = row.querySelector(".btn-primary");
-                editButton.disabled = true;
-                saveButton.disabled = true;
+                console.log("Database updated successfully:", data);
             })
             .catch((error) => {
-                console.error("Error fetching boolean value:", error);
+                console.error("Error updating database:", error);
             });
-    });
-    const modal = document.getElementById("Alert!");
-    const modalBody = modal.querySelector(".modal-body");
-    modalBody.innerHTML = `<p style="border:2px solid rgba(229, 231, 235);border-radius:15px;padding:20px;background-color:rgba(234, 245, 220);">You have edited this text successfully!<br/>And it can't be edited anymore.</p>`;
-
-    modal.classList.add("show");
-    modal.style.display = "block";
-
-    const closeButton = document.querySelector("#closeButtonAlert");
-    closeButton.addEventListener("click", function () {
-        modal.classList.remove("show");
-        modal.style.display = "none";
-    });
-}
-
-function viewDescription(row) {
-    const englishDescription = row.cells[3].textContent;
-    const arabicDescription = row.cells[2].textContent;
-    const itemCode = row.cells[1].textContent;
-    displayTranslationDescription(
-        itemCode,
-        englishDescription,
-        arabicDescription
-    );
-}
-
-function displayTranslationDescription(
-    itemCode,
-    englishDescription,
-    arabicDescription
-) {
-    const modal = document.getElementById("EnglishDescription");
-    const modalBody = modal.querySelector(".modal-body");
-
-    modalBody.innerHTML = `
-  <p><strong style="color:rgba(1, 86, 66)">Item Code:</strong><p style="border:2px solid rgba(229, 231, 235);border-radius:15px;padding:20px;background-color:rgba(234, 245, 220);">${itemCode}</p></p>
-  <p><strong style="color:rgba(1, 86, 66)">English Description:</strong> <p style="border:2px solid rgba(229, 231, 235);border-radius:15px;padding:20px;background-color:rgba(234, 245, 220);" >${englishDescription}</p></p>
-  <p><strong style="color:rgba(1, 86, 66)">Arabic Description:</strong> <p style="border:2px solid rgba(229, 231, 235);border-radius:15px;padding:20px;background-color:rgba(234, 245, 220)" >${arabicDescription}</p></p>
-`;
-    modal.classList.add("show");
-    modal.style.display = "block";
-
-    const closeButton = document.querySelector("#closeButton");
-    closeButton.addEventListener("click", function () {
-        modal.classList.remove("show");
-        modal.style.display = "none";
-    });
-}
-
-function updatePagination() {
-    const pagination = document.getElementById("pagination");
-    pagination.innerHTML = "";
-    const totalPages = Math.ceil(display.length / itemsPerPage);
-    for (let i = 1; i <= totalPages; i++) {
-        const li = document.createElement("li");
-        li.classList.add("page-item");
-        const link = document.createElement("a");
-        link.classList.add("page-link");
-        link.textContent = i;
-        link.href = "#";
-        link.addEventListener("click", function () {
-            currentPage = i;
-            displayTableRows();
-            updatePagination();
-        });
-        li.appendChild(link);
-        pagination.appendChild(li);
     }
-}
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("http://localhost:8000/api/translation")
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then((data) => {
-            display = data.data;
-            displayTableRows();
-            updatePagination();
-        })
-        .catch((error) => console.error("Error fetching data:", error));
 });
-document.getElementById("exportButton").addEventListener("click", function () {
-    exportToCsv("translate_users.csv");
-});
-
-function exportToCsv(filename) {
-    const table = document.getElementById("table_output");
-    const header = [
-        "ID",
-        "Item Code",
-        "Arabic Translation",
-        "English Translation",
-        "Username",
-    ];
-    const rows = [header];
-
-    for (const row of table
-        .getElementsByTagName("tbody")[0]
-        .getElementsByTagName("tr")) {
-        const rowData = [];
-
-        // Fetch specific data using getElementById or any other method you prefer
-        const id = row.querySelector("#wanted_id").textContent.trim();
-        const itemCode = row
-            .querySelector("#wanted_item_code")
-            .textContent.trim();
-        const arabicTranslation = row
-            .querySelector("#wanted_arabic_translation")
-            .textContent.trim();
-        const englishTranslation = row
-            .querySelector("#wanted_english_translation")
-            .textContent.trim();
-        const username = row
-            .querySelector("#wanted_username")
-            .textContent.trim();
-        rowData.push(
-            id,
-            itemCode,
-            arabicTranslation,
-            englishTranslation,
-            username
-        );
-
-        rows.push(rowData);
-    }
-
-    const csvContent = Papa.unparse(rows, { encoding: "UTF-8" });
-    const blob = new Blob([String.fromCharCode(0xfeff), csvContent], {
-        type: "text/csv;charset=utf-8;",
-    });
-    const link = document.createElement("a");
-    if (link.download !== undefined) {
-        const url = URL.createObjectURL(blob);
-        link.setAttribute("href", url);
-        link.setAttribute("download", filename);
-        link.style.visibility = "hidden";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
-}
-
-function updateDatabaseText(itemId, updatedText, updatedBooleanValue) {
-    let url = `http://localhost:8000/api/update-text/${itemId}`;
-    console.log("itemID: " + itemId);
-    console.log(updatedBooleanValue);
-    let data = {
-        text: updatedText,
-        booleanValue: updatedBooleanValue,
-        // arabic: updatedArabic,
-    };
-    fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]')
-                .content,
-        },
-        body: JSON.stringify(data),
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log("Database updated successfully:", data);
-        })
-        .catch((error) => {
-            console.error("Error updating database:", error);
-        });
-}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,24 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="icon" href="/exceIcon.png" type="favicon/ico" />
+    <script
+      type="text/javascript"
+      src="https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js"
+      defer
+    >
+  </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.0/papaparse.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <title>Excel File Importer with Dynamic Table</title>
@@ -79,6 +94,183 @@
       </div>
   </div>
 </div>
+<script>
+  const itemsPerPage = 5;
+let currentPage = 1;
+
+function displayTableRows() {
+    const tableBody = document.getElementById("table_body");
+    tableBody.innerHTML = "";
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const displayedItems = display.slice(startIndex, endIndex);
+
+    displayedItems.forEach((item) => {
+        const row = document.createElement("tr");
+        // console.log(item.is_updated==1);
+        if (item.is_updated == 1) {
+            row.classList.add("table-secondary");
+            // console.log(row.classList);
+            row.innerHTML = `
+          <td id="wanted_id">${item.id}</td>
+          <td id="wanted_item_code">${item.item_code}</td>
+          <td id="wanted_arabic_translation"><textarea class="table-secondary form-control" rows="8" cols="120" disabled style=resize:none>${item.arabic_translation}</textarea></td>
+          <td id="wanted_english_translation"><textarea class="table-secondary form-control" rows="8" cols="120" disabled style=resize:none>${item.english_translation}</textarea></td>
+          <td id="wanted_username">${item.username}</td>
+          <td>
+            <button class="btn btn-success" disabled onclick="handleEditButtonClick(this.parentNode.parentNode)">Edit</button>
+            <button class="btn btn-primary" disabled onclick="handleSaveButtonClick(this.parentNode.parentNode)">Save</button>
+            <button class="btn btn-primary" onclick="viewDescription(this.parentNode.parentNode)">View</button>
+          </td>
+        `;
+        } else {
+            row.innerHTML = `
+        <td id="wanted_id">${item.id}</td>
+        <td id="wanted_item_code">${item.item_code}</td>
+        <td id="wanted_arabic_translation"><textarea class="form-control" rows="8" cols="120" disabled style=resize:none>${item.arabic_translation}</textarea></td>
+        <td id="wanted_english_translation"><textarea class="form-control" rows="8" cols="120" disabled style=resize:none>${item.english_translation}</textarea></td>
+        <td id="wanted_username">${item.username}</td>
+        <td>
+          <button class="btn btn-success" onclick="handleEditButtonClick(this.parentNode.parentNode)">Edit</button>
+          <button class="btn btn-primary" onclick="handleSaveButtonClick(this.parentNode.parentNode)">Save</button>
+          <button class="btn btn-primary" onclick="viewDescription(this.parentNode.parentNode)">View</button>
+        </td>
+      `;
+        }
+        tableBody.appendChild(row);
+    });
+}
+
+function handleEditButtonClick(row) {
+    const textareas = row.querySelectorAll("textarea");
+    textareas.forEach((textarea) => {
+        textarea.disabled = false;
+        textarea.style.color = "black";
+        textarea.style.background = "white";
+    });
+}
+
+function handleSaveButtonClick(row) {
+    console.log(row);
+    // row.style.backgroundColor = "lightgray";
+    const textareas = row.querySelectorAll("textarea");
+    textareas.forEach((textarea) => {
+        textarea.disabled = true;
+        textarea.style.background = "lightgray";
+        const ID = row.querySelector("#wanted_id").innerText;
+        console.log("Item ID is: " + ID);
+        const itemId = ID;
+        const updatedText = textarea.value;
+        console.log("Updated Text is " + updatedText);
+        fetch(`http://localhost:8000/api/get-boolean-value/${itemId}`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                let updatedBooleanValue = data.is_updated;
+                updatedBooleanValue = "1";
+                console.log("bol val: " + updatedBooleanValue);
+                updateDatabaseText(itemId, updatedText, updatedBooleanValue);
+                const editButton = row.querySelector(".btn-success");
+                const saveButton = row.querySelector(".btn-primary");
+                editButton.disabled = true;
+                saveButton.disabled = true;
+            })
+            .catch((error) => {
+                console.error("Error fetching boolean value:", error);
+            });
+    });
+    const modal = document.getElementById("Alert!");
+    const modalBody = modal.querySelector(".modal-body");
+    modalBody.innerHTML = `<p style="border:2px solid rgba(229, 231, 235);border-radius:15px;padding:20px;background-color:rgba(234, 245, 220);">You have edited this text successfully!<br/>And it can't be edited anymore.</p>`;
+
+    modal.classList.add("show");
+    modal.style.display = "block";
+
+    const closeButton = document.querySelector("#closeButtonAlert");
+    closeButton.addEventListener("click", function () {
+        modal.classList.remove("show");
+        modal.style.display = "none";
+    });
+}
+
+function viewDescription(row) {
+    const englishDescription = row.cells[3].textContent;
+    const arabicDescription = row.cells[2].textContent;
+    const itemCode = row.cells[1].textContent;
+    displayTranslationDescription(
+        itemCode,
+        englishDescription,
+        arabicDescription
+    );
+}
+
+function displayTranslationDescription(
+    itemCode,
+    englishDescription,
+    arabicDescription
+) {
+    const modal = document.getElementById("EnglishDescription");
+    const modalBody = modal.querySelector(".modal-body");
+
+    modalBody.innerHTML = `
+  <p><strong style="color:rgba(1, 86, 66)">Item Code:</strong><p style="border:2px solid rgba(229, 231, 235);border-radius:15px;padding:20px;background-color:rgba(234, 245, 220);">${itemCode}</p></p>
+  <p><strong style="color:rgba(1, 86, 66)">English Description:</strong> <p style="border:2px solid rgba(229, 231, 235);border-radius:15px;padding:20px;background-color:rgba(234, 245, 220);" >${englishDescription}</p></p>
+  <p><strong style="color:rgba(1, 86, 66)">Arabic Description:</strong> <p style="border:2px solid rgba(229, 231, 235);border-radius:15px;padding:20px;background-color:rgba(234, 245, 220)" >${arabicDescription}</p></p>
+`;
+    modal.classList.add("show");
+    modal.style.display = "block";
+
+    const closeButton = document.querySelector("#closeButton");
+    closeButton.addEventListener("click", function () {
+        modal.classList.remove("show");
+        modal.style.display = "none";
+    });
+}
+
+function updatePagination() {
+    const pagination = document.getElementById("pagination");
+    pagination.innerHTML = "";
+    const totalPages = Math.ceil(display.length / itemsPerPage);
+    for (let i = 1; i <= totalPages; i++) {
+        const li = document.createElement("li");
+        li.classList.add("page-item");
+        const link = document.createElement("a");
+        link.classList.add("page-link");
+        link.textContent = i;
+        link.href = "#";
+        link.addEventListener("click", function () {
+            currentPage = i;
+            displayTableRows();
+            updatePagination();
+        });
+        li.appendChild(link);
+        pagination.appendChild(li);
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+        fetch("http://localhost:8000/api/translation")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                display = data.data;
+                displayTableRows();
+                updatePagination();
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+                // Handle error fetching data
+            });
+    });
+</script>
 <button style=" margin: 0px 170px  " id="exportButton" class="btn btn-primary">Export as CSV</button>
 @endsection
 </body>
